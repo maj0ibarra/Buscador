51aee662a57558f1d542b062caa7db8a8ebfb617
marialexculturaapp <maria.ibarra@cbtis72.edu.mx>
2019-08-16 00:58:53 -0500

    Initial commit

---
+++ app/src/main/java/alejandro/myapplication/Dos.java
@@ -0,0 +1,13 @@
+package alejandro.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dos extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dos);
+    }
+}
---
+++ app/src/main/java/alejandro/myapplication/Nueva.java
@@ -0,0 +1,13 @@
+package alejandro.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Nueva extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva);
+    }
+}
